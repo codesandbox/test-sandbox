51564d9925bf711bc1cef7887dfe5b191b679b69
Maria Clara <claarasantana@gmail.com>
2022-02-03 17:42:59 +0000

    test

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -3,11 +3,12 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
   name: string
+  username: string
 }
 
 export default function handler(
   _: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: 'John Doe', username: "johndoe" })
 }
