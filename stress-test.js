a7217bb2c55c3bc16c7e3584c837cce510f55815
Christian Alfoni <christianalfoni@gmail.com>
2022-02-17 09:06:10 +0000

    Create stress-test.js and test.json

---
+++ stress-test.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+
+function wait(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+  
+
+
+
+async function run() {
+  await wait(randomIntFromInterval(500, 2000))
+  const json = fs.readFileSync('test.json').toString()
+  try {
+      const parsedJson = JSON.parse(json)
+      parsedJson[Date.now()] = {
+          foo: 'bar'
+      }
+      fs.writeFileSync('test.json', JSON.stringify(parsedJson, null, 2))
+  } catch {
+    console.log("Could not parse")
+  }
+  
+  run()
+}
+
+run()
