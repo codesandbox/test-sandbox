e45438c5073b18b1fca329ee70584078acc30bbe
Jasper De Moor <jasperdemoor@gmail.com>
2022-03-01 13:44:04 +0000

    test2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,9 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  //test
-  return <Component {...pageProps} />
+  // test
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
