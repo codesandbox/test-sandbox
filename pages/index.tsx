478a3fc0bb6240acac1a638ce6eff6c212950443
Gianmarco <gsimone@hey.com>
2022-04-20 15:09:44 +0000

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,8 +18,8 @@ const Home: NextPage = () => {
 
         <p className={styles.description}>
           Start editingg
-          <code className={styles.code}>pages/index.tsx</code> to see some magic
-          happen ✨.
+          <code className={styles.code}>pages/index.tsx</code> toad see some
+          magic happen ✨.
         </p>
 
         <div className={styles.grid}>
