c393efb48f4c12b1531113e3450bec50a50bcd1c
Danilo Woznica <danilowoz@gmail.com>
2022-05-19 12:49:37 +0000

    Update 8 files

---
+++ demo-1/App.js
@@ -0,0 +1,22 @@
+import { sum } from "lodash";
+
+let beep = false;
+setInterval(() => {
+  const thingie = document.getElementById("nice-text");
+
+  if (thingie) {
+    thingie.style.color = beep ? "blue" : "tomato";
+    thingie.style.fontSize = (beep ? 10 : 100) + "px";
+  }
+
+  beep = !beep;
+}, 600);
+
+export default () => (
+  <h1>
+    <div id="nice-text" style={{ color: "tomato", transition: "2s ease all" }}>
+      I need to make changes for Christian!!!!!!!!!!!
+    </div>
+    {sum([2, 2])}
+  </h1>
+);
--- demo-1/index.js
+++ demo-1/index.js
@@ -1,6 +1,5 @@
 import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
-import "./styles.css";
 
 import App from "./App";
 
