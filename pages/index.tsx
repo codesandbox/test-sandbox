c9b733b8b5a9a03fbe8ff437bd1b65c839375333
Christian Alfoni <christianalfoni@gmail.com>
2023-02-07 10:27:49 +0000

    Update package.json and index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import styles from "../styles/Home.module.css";
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      <h1>Hello CodeSandbox!</h1>
+      <h1>Hello Me :) </h1>
       <h2>This is Next.js</h2>
     </div>
   );
