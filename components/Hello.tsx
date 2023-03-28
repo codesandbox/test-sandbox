7857efffc4ffeec50114a6bc24d391f3c6623365
Testing <davidhollins06@gmail.com>
2023-03-28 13:03:49 +0000

    Update .eslintrc.json and Hello.tsx

--- components/Hello.tsx
+++ components/Hello.tsx
@@ -6,4 +6,4 @@ export const Hello = ({
 }: {
   name: string;
   className: string;
-}) => <h1 className={classNamedssasdddsd}>Hello {name}!</h1>;
+}) => <h1 className={classNamedssasdddsssd}>Hello {name}!</h1>;
