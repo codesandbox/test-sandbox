131eb18eb9074cc747b6369145e32be34a97c2ed
Testing <davidhollins06@gmail.com>
2023-03-28 10:52:44 +0000

    Update 3 files

--- components/Hello.tsx
+++ components/Hello.tsx
@@ -6,4 +6,4 @@ export const Hello = ({
 }: {
   name: string;
   className: string;
-}) => <h1 className={classNameddsssasssd}>Hello {name}!</h1>;
+}) => <h1 className={classNamedsssasssd}>Hello {name}!</h1>;
