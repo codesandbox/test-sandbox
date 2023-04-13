928a354a1b737aea0560ddf353191e7687010e13
Testing <alex.n.moldovan@gmail.com>
2023-04-13 10:40:07 +0000

    feat: Add setup and task commands to CodeSandbox project and create new test page with link to home page

--- pages/test.tsx
+++ pages/test.tsx
@@ -2,13 +2,13 @@ import type { NextPage } from "next";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
-const Index: NextPage = () => {
+const TestPage: NextPage = () => {
   return (
     <div className={styles.container}>
-      <h1>Index</h1>
-      <Link href="/test">GO test</Link>
+      <h1>Test</h1>
+      <Link href="/">GO home</Link>
     </div>
   );
 };
 
-export default Index;
+export default TestPage;
