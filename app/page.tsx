db2a258f96377f89f6523fd0d2a416022a652527
Testing <alex.n.moldovan@gmail.com>
2023-06-23 10:29:34 +0000

    new stuff

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
+            target="="
             rel="noopener noreferrer"
           >
             By{" "}
