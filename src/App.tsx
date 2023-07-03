5385912482eb7b04bfcc8f64d0410887e959e272
Filipe Lima <filipe@codesandbox.io>
2023-07-03 12:06:18 +0000

    change project test

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+interface AppProps {
+  // Define the types for any props your component might receive
+}
+
+function App(props: AppProps) {
+  const [count, setCount] = useState<number>(0);
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1>React + Vite</h1>
+      <h2>On CodeSandbox!</h2>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR.
+        </p>
+
+        <p>
+          Tip: you can use the inspector button next to the address bar to click
+          on components in the preview and open the code in the editor!
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
