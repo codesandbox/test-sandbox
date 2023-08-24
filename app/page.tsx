9530c59334f9e4451fd05c2666b8726cd3e5ae6f
Testing <christianalfoni@gmail.com>
2023-08-24 09:35:51 +0000

    style: update page component text style

--- app/page.tsx
+++ app/page.tsx
@@ -72,7 +72,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+            Making a change!
           </p>
         </a>
 
