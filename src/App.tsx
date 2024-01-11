7e14355a59e50ea13ba9d96a429eed123905ef4d
Christian Alfoni <christianalfoni@gmail.com>
2024-01-11 11:09:10 +0000

    fix: Fix count initialization

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(100);
+  const [count, setCount] = useState(200);
 
   return (
     <>
