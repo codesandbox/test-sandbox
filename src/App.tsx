9735cf53d4d7634a61e40ac1fcf02f7368a1886f
Christian Alfoni <christianalfoni@gmail.com>
2024-03-12 08:02:10 +0000

    Just testing

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { foo } from "./test";
 
 function App() {
   const [count, setCount] = useState(10);
@@ -19,7 +20,7 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is {count} {foo}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
