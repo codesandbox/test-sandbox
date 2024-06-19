c37af6f9771a31d8c6faf9c91bdf375f67cd7b7a
Joji Augustine <jojimail@gmail.com>
2024-06-19 13:47:49 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edits <code>src/App.tsx</code> and save to test HMR
+          Editss <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
